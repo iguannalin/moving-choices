2963b50157178cf8cac3d96ca624c5d7e7bcf760
seasons986 <annalin12345@gmail.com>
2023-06-18 16:49:15 -0400

    yeah

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-window.addEventListener("load", () => {});
+window.addEventListener("load", () => {
+  let w = window.innerWidth - 200;
+  let h = window.innerHeight - 200;
+  const parent = document.getElementById("parent");
+  let hoverArea = document.createElement("div");
+  hoverArea.className = "hover-area";
+  let input = document.createElement("input");
+  
+  input.type = "checkbox";
+  reposition(hoverArea);
+  hoverArea.appendChild(input);
+  parent.appendChild(hoverArea);
+  
+  hoverArea.onmouseover = () => {reposition(hoverArea)};
+  hoverArea.onclick = () => {reposition(hoverArea)};
+
+  function reposition(elem) {
+    console.log(Math.floor(Math.random(0,w)));
+    elem.style.top = `${getRandomInt(w)}px`;
+    elem.style.left = `${getRandomInt(h)}px`;
+  }
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+});
